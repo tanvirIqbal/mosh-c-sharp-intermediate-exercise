52360002505ca0d098eb497e98267901f8f5a88d
tanviriqbal <tanviriqbal4u@live.com>
2018-05-14 06:19:52 +0600

    Stopwatch

---
+++ StopWatch/StopWatch/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Press 1 to Start the watch\nPress 2 to stop the watch\nPress 0 to exit the program.");
+            var userInput = Convert.ToInt32(Console.ReadLine());
+            StopWatch stopWatch = new StopWatch();
+            bool isStarted = false;
+            while (userInput != 0)
+            {
+                if (userInput == 1)
+                {
+                    stopWatch.Start();
+                    if (isStarted)
+                    {
+                        throw new InvalidOperationException("Stop Watch is already running");
+                    }
+                    isStarted = true;
+                    Console.WriteLine("Started. Press 2 to Stop or 0 to exit.");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                }
+                else if (userInput == 2)
+                {
+                    if (isStarted)
+                    {
+                        stopWatch.Stop();
+                        isStarted = false;
+                        Console.WriteLine("Stopped. Press 1 to Start or 0 to exit.");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Start the watchor 0 to exit.");
+                    }
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                }
+            }
+        }
+    }
+}
---
+++ StopWatch/StopWatch/StopWatch.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StopWatch
+{
+    public class StopWatch
+    {
+        private DateTime start;
+        private DateTime end;
+
+        public void Start()
+        {
+            start = DateTime.Now;
+        }
+
+        public void Stop()
+        {
+            end = DateTime.Now;
+            TimeSpan duration = end - start;
+            Console.WriteLine(duration.Hours+":"+duration.Minutes+":"+duration.Seconds);
+        }
+    }
+}
